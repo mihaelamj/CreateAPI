62b89c98904e092c722784a6c15762dec811213d
Mihaela MJ <mihaelamj@me.com>
2023-05-23 13:54:05 +0200

    Added extended petsore with enum

--- Tests/Support/Snapshots/petstore-no-package/Entities/Pet.swift
+++ Tests/Support/Snapshots/petstore-no-package/Entities/Pet.swift
@@ -10,11 +10,26 @@ public struct Pet: Codable {
     public var id: Int64
     /// Example: "Buddy"
     public var name: String
+    /// Kind of animal
+    ///
+    /// Example: "bird"
+    public var kind: Kind?
     public var tag: String?
 
-    public init(id: Int64, name: String, tag: String? = nil) {
+    /// Kind of animal
+    ///
+    /// Example: "bird"
+    public enum Kind: String, Codable, CaseIterable {
+        case dog
+        case cat
+        case bird
+        case fish
+    }
+
+    public init(id: Int64, name: String, kind: Kind? = nil, tag: String? = nil) {
         self.id = id
         self.name = name
+        self.kind = kind
         self.tag = tag
     }
 
@@ -22,6 +37,7 @@ public struct Pet: Codable {
         let values = try decoder.container(keyedBy: StringCodingKey.self)
         self.id = try values.decode(Int64.self, forKey: "id")
         self.name = try values.decode(String.self, forKey: "name")
+        self.kind = try values.decodeIfPresent(Kind.self, forKey: "kind")
         self.tag = try values.decodeIfPresent(String.self, forKey: "tag")
     }
 
@@ -29,6 +45,7 @@ public struct Pet: Codable {
         var values = encoder.container(keyedBy: StringCodingKey.self)
         try values.encode(id, forKey: "id")
         try values.encode(name, forKey: "name")
+        try values.encodeIfPresent(kind, forKey: "kind")
         try values.encodeIfPresent(tag, forKey: "tag")
     }
-}
+
